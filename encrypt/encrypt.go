7b762c3fbc2cdf7839e061b07f858bfcb4fe38af
zhoushuai <zhoushuai@luojilab.com>
2019-10-13 16:15:37 +0800

    update encrypt and add encrypt test file

--- encrypt/encrypt.go
+++ encrypt/encrypt.go
@@ -15,32 +15,47 @@ import (
 	"io/ioutil"
 )
 
+//var aesCommonIV = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
+//var aesCommonKey128 = []byte{0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c}
+
+// 可以用的key做iv， 也可以自定义传入
 
 // AesEncrypt Aes加密
-func AesEncrypt(origData, key []byte) ([]byte, error) {
+func AesEncrypt(origData, key []byte, iv ...[]byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 	blockSize := block.BlockSize()
 	origData = PKCS5Padding(origData, blockSize)
-	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
+	aesIV := key[:blockSize]
+	if len(iv) > 0 {
+		aesIV = iv[0]
+	}
+	blockMode := cipher.NewCBCEncrypter(block, aesIV)
 	crypted := make([]byte, len(origData))
 	blockMode.CryptBlocks(crypted, origData)
 	return crypted, nil
 }
 
 // AesDecrypt Aes解密
-func AesDecrypt(crypted, key []byte) ([]byte, error) {
+func AesDecrypt(crypted, key []byte, iv ...[]byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 	blockSize := block.BlockSize()
-	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
+	aesIV := key[:blockSize]
+	if len(iv) > 0 {
+		aesIV = iv[0]
+	}
+	blockMode := cipher.NewCBCDecrypter(block, aesIV)
 	origData := make([]byte, len(crypted))
 	blockMode.CryptBlocks(origData, crypted)
-	origData = PKCS5UnPadding(origData)
+	if origData = PKCS5UnPadding(origData); len(origData) == 0 {
+		return nil, errors.New("aes decrypt data failed,wrong aesKey or data")
+	}
+
 	return origData, nil
 }
 
@@ -55,6 +70,9 @@ func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
 func PKCS5UnPadding(origData []byte) []byte {
 	length := len(origData)
 	unpadding := int(origData[length-1])
+	if length < unpadding { // Maybe slice bounds out of range,check origData and aeskey
+		return []byte{}
+	}
 	return origData[:(length - unpadding)]
 }
 
@@ -76,7 +94,7 @@ func ReadFile(filename string) ([]byte, error) {
 }
 
 // RsaEncrypt Rsa加密 公钥加密
-func RsaEncrypt(rawData []byte,publicKeyPath string) ([]byte, error) {
+func RsaEncrypt(rawData []byte, publicKeyPath string) ([]byte, error) {
 
 	publicKey, err := ioutil.ReadFile(publicKeyPath) //A successful call returns err == nil
 	if err != nil {
@@ -96,7 +114,7 @@ func RsaEncrypt(rawData []byte,publicKeyPath string) ([]byte, error) {
 }
 
 // RsaDecrypt Rsa解密 私钥解密
-func RsaDecrypt(ciphertext []byte,privateKeyPath string) ([]byte, error) {
+func RsaDecrypt(ciphertext []byte, privateKeyPath string) ([]byte, error) {
 	privateKey, err := ioutil.ReadFile(privateKeyPath) //A successful call returns err == nil
 	if err != nil {
 		return nil, err
--- encrypt/encrypt_test.go
+++ encrypt/encrypt_test.go
@@ -1,25 +1,66 @@
-package encrypt
+package encrypt_test
 
 import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"testing"
+	"zhoushuai.com/GolangTraining/encrypt"
 )
+
+var aesCommonKey128 = []byte{0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c}
+var aesCommonIV = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
+
+// TestAesEncrypt ...
+func TestAesEncrypt(t *testing.T) {
+	originalText := "admin"
+	fmt.Println("originalText:", originalText)
+	mytext := []byte(originalText)
+	cryptoText, err := encrypt.AesEncrypt(mytext, aesCommonKey128, aesCommonIV)
+	if !assert.Nil(t, err, fmt.Sprintf("aes encrypt error:%v", err)) {
+		return
+	}
+	fmt.Println("cryptoText hex:", hex.EncodeToString(cryptoText))
+	fmt.Println("cryptoText base64:", base64.StdEncoding.EncodeToString(cryptoText))
+
+}
+
+// TestAesDecryption ...
+func TestAesDecryption(t *testing.T) {
+	originalText := "QH7yT5TDkOGNRmSZC6htpQ=="
+	//originalText := "dCjlRtxsBp66W0Hbe2TD8g=="
+	byteData, err := base64.StdEncoding.DecodeString(originalText)
+	if !assert.Nil(t, err, fmt.Sprintf("encrypted data base64 decode error:%v", err)) {
+		return
+	}
+	decryptedText, err := encrypt.AesDecrypt(byteData, aesCommonKey128, aesCommonIV)
+	if !assert.Nil(t, err, fmt.Sprintf("aes decrypt error:%v", err)) {
+		return
+	}
+	expected := "admin"
+	acutal := string(decryptedText)
+	fmt.Println("decryptedText:", string(decryptedText))
+	assert.Equal(t, expected, acutal, "not match with actual data")
+}
+
 // TestRSAEncrypt ...
 func BenchmarkRSAEncrypt(t *testing.B) {
 	// encrtypt data
 	//data := []byte("joshua")
 	publicKeyPath := "../public.pem"
 
-	for i:=0 ;i<100000;i++ {
+	for i := 0; i < 100000; i++ {
 		data, _ := ioutil.ReadFile("access.log")
-		res, err := RsaEncrypt(data, publicKeyPath)
+		res, err := encrypt.RsaEncrypt(data, publicKeyPath)
 		if err != nil {
 			t.Error(err)
 			return
 		}
 		// decrypt data
 		privatePath := "../private.pem"
-		res, err = RsaDecrypt(res, privatePath)
+		res, err = encrypt.RsaDecrypt(res, privatePath)
 		if err != nil {
 			t.Error(err)
 			return
@@ -36,14 +77,14 @@ func BenchmarkAesEncrypt(t *testing.B) {
 	key := []byte("aeTh6ae2eu2phoob")
 	// encrtypt data
 	data, _ := ioutil.ReadFile("stderr.log")
-	res, err := AesEncrypt(data, key)
+	res, err := encrypt.AesEncrypt(data, key)
 	if err != nil {
 		t.Error(err)
 		return
 	}
-    //t.Log("encrypt data :",string(res))
+	//t.Log("encrypt data :",string(res))
 	// decrypt data
-	res, err = AesDecrypt(res, key)
+	res, err = encrypt.AesDecrypt(res, key)
 	if err != nil {
 		t.Error(err)
 		return
